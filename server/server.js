27ee49569e0611cb94ab0cb9e712fa306d12a4fa
birma612 <birmabm267@gmail.com>
2026-03-15 13:56:06 +0530

    vercel  fixed

--- server/server.js
+++ server/server.js
@@ -1,23 +1,46 @@
-import 'dotenv/config';
-import express from 'express';
-import cors from 'cors';
-import connectDB from './config/mongodb.js';
+// import 'dotenv/config';
+// import express from 'express';
+// import cors from 'cors';
+// import connectDB from './config/mongodb.js';
 
 
 
-// App config
+// // App config
 
-const PORT= process.env.PORT|| 4000
-const app= express()
-await connectDB()
-//Intialize misddlewares
+// const PORT= process.env.PORT|| 4000
+// const app= express()
+// await connectDB()
+// //Intialize misddlewares
+// app.use(express.json())
+// app.use(cors())
+
+// // API routes
+
+// app.get('/',(req,res)=>{
+//     res.send("API is working ")
+
+// })
+// app.listen(PORT,()=>console.log("server is running at",PORT))
+
+
+import 'dotenv/config'
+import express from 'express'
+import cors from 'cors'
+import connectDB from './config/mongodb.js'
+
+const app = express()
+
+// middleware
 app.use(express.json())
 app.use(cors())
 
-// API routes
+// database connection
+connectDB()
 
-app.get('/',(req,res)=>{
-    res.send("API is working ")
-
+// routes
+app.get('/', (req, res) => {
+  res.send("API is working")
 })
-app.listen(PORT,()=>console.log("server is running at",PORT))
+
+// export app for Vercel
+export default app
